92263363e672feacc2be916a1681332f972e99c8
Amal <amalprasad354@gmail.com>
2026-04-12 16:03:38 +0530

    Add Docker configuration, environment settings, and update application structure

--- backend/taskflow/Program.cs
+++ backend/taskflow/Program.cs
@@ -3,10 +3,8 @@ using Npgsql;
 DotNetEnv.Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
